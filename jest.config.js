35da1b7175b174dc3eac82359528eec2d87eabcc
Anar Allakhverdiev <wiseadme@gmail.com>
2022-03-01 02:04:37 +0300

    test cases

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  verbose: true,
+  roots: [
+    '<rootDir>/tests',
+  ],
+  moduleFileExtensions: [
+    'vue',
+    'ts',
+    'js',
+  ],
+  moduleDirectories: [
+    'node_modules',
+  ],
+
+  transform: {
+    '.*\\.(j|t)s$': 'ts-jest',
+    '.*\\.ts$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!**/*.d.ts',
+  ],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
+  testMatch: [
+    // Default
+    '**/tests/**/*.ts',
+  ],
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+      // tsConfig: '<rootDir>/tsconfig.test.json',
+      diagnostics: false,
+    },
+  },
+}
---
+++ tests/createStore.spec.ts
@@ -0,0 +1,85 @@
+import { createStore } from '../src/index'
+// import { isAsyncFunction } from '../src/helpers'
+import 'regenerator-runtime/runtime'
+
+describe('createStore', () => {
+  const user = { name: 'Alex' }
+  let store
+  let storeOptions
+
+  beforeEach(() => {
+    storeOptions = {
+      state: {
+        user: null as any
+      },
+      mutations: {
+        setUser: (state, user) => state.user = user
+      },
+      actions: {
+        async fetchUser({ commit }) {
+          const data = await new Promise(resolve => setTimeout(() => resolve(user), 1000))
+          commit('setUser', data)
+          return data
+        },
+
+        async callAnotherAction({ dispatch }) {
+          const data = await dispatch('fetchUser')
+          return data
+        },
+
+        mutateStateFromAction({ state }, user) {
+          state.user = user
+        }
+      }
+    }
+    store = createStore(storeOptions)
+  })
+
+  it('should create store', () => {
+    expect(store.state).toBeTruthy()
+    expect(store.dispatch).toBeTruthy()
+    expect(store.commit).toBeTruthy()
+  })
+
+  it('should test the "commit" executing inside the action', async () => {
+    const stub = jest.fn()
+    storeOptions.mutations.setUser = stub
+    await store.dispatch('fetchUser')
+    expect(stub).toHaveBeenCalled()
+    expect(stub).toBeCalledWith(storeOptions.state, user)
+  })
+
+  it('should test "state" mutation', async () => {
+    await store.dispatch('fetchUser')
+    expect(store.state.user.name).toEqual(user.name)
+  })
+
+  it('should test the "dispatch" executing inside the action', async () => {
+    const stub = jest.fn()
+    storeOptions.actions.fetchUser = stub
+    await store.dispatch('callAnotherAction')
+    expect(stub).toHaveBeenCalled()
+  })
+
+  it('should change "state" from action without calling mutation', () => {
+    store.dispatch('mutateStateFromAction', user)
+
+    expect(store.state.user.name).toEqual(user.name)
+  })
+
+  it('should log error on async mutation', async () => {
+    const spyOnError = jest.spyOn(console, 'error')
+    storeOptions.mutations.setUser = async () => true
+    await store.commit('setUser')
+
+    expect(spyOnError).toHaveBeenCalled()
+  })
+
+  it('should log error on "setTimeout" in mutations', () => {
+    const spyOnError = jest.spyOn(console, 'error')
+    storeOptions.mutations.setUser = () => setTimeout(() => true)
+    store.commit('setUser')
+
+    expect(spyOnError).toHaveBeenCalled()
+  })
+})
